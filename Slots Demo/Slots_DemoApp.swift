7fd8e1d5b9a63dafd02874148688cf278da65c33
KenFai <ngkenfai@gmail.com>
2020-10-22 15:40:33 +0800

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Ginger on 22/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
